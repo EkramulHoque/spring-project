bacbb151e287b398d8c7ab4ea8052ea90dc14e20
ehoque <ehoque@teamamericany.com>
2016-10-18 17:08:47 -0700

    second commit

--- src/main/java/org/spring/ekram/Drawing.java
+++ src/main/java/org/spring/ekram/Drawing.java
@@ -1,6 +1,8 @@
 package org.spring.ekram;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
 /**
@@ -8,9 +10,13 @@ import org.springframework.core.io.FileSystemResource;
  */
 public class Drawing {
     public static void main(String [] args){
-    BeanFactory factory= new XmlBeanFactory(new FileSystemResource("spring.xml"));
+   /* BeanFactory factory= new XmlBeanFactory(new FileSystemResource("spring.xml"));
         Triangle triangle=(Triangle) factory.getBean("Triangle");
         triangle.draw();
+*/
+        ApplicationContext app= new ClassPathXmlApplicationContext("spring.xml");
+        Triangle triangle1= (Triangle) app.getBean("Triangle");
+        triangle1.draw();
 
     }
 }
